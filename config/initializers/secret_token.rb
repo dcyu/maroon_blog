b4a6a5df7489c268ca11c5c12dbb661c03723f52
Daniel Yu <dcyu93@gmail.com>
2014-05-14 16:10:08 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MaroonBlog::Application.config.secret_key_base = '2078c367c29b85218e9b6a5b94b6c1c0623bbc9790fcf609aa1a9b41db088de8635ebc914c3dfa7e4106eaa7ba458192bb5fa7c3ca68bea715630f6e028d82df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MaroonBlog::Application.config.session_store :cookie_store, key: '_maroon_blog_session'
